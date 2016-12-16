4146edadb839cde9d025d3ff7e77545ae21e2dd4
Lidiya Nikolova <lidiya@Lidiyas-MacBook-Pro.local>
2016-12-16 11:33:47 +0000

    Clean up quotation marks and change word

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 			
 var words = [
 		// Russian, Korean, Spanish
-		['бабушка', '할머니', 'abuela'],
-	        ['почему', '유', 'por qué'],
+		    ['бабушка', '할머니', 'abuela'],
+	        ['почему', '왜', 'por qué'],
 	        ['большой', '크', 'grande'],
-	        // You can add here a word and its translation
+	        
 	    ];
 
 $(document).ready(function randomize() {
 	var i = Math.floor(Math.random() * words.length); //Random number
-	$("#ru-word").append(words[i][0]);
-	$("#kr-word").append(words[i][1]);
-	$("#es-word").append(words[i][2]);
+	$('#ru-word').append(words[i][0]);
+	$('#kr-word').append(words[i][1]);
+	$('#es-word').append(words[i][2]);
 
 	$('body').click(function(){
 		location.reload();
