ce6b935cef1413f4c5aaf28ed7fe515592a052f6
Lidiya Nikolova <lidiya@Lidiyas-MacBook-Pro.local>
2016-12-22 12:17:27 +0000

    Finish translations

--- script.js
+++ script.js
@@ -11,31 +11,34 @@ var russianWords = [
 ];
 
 var koreanWords = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+    ['하늘', 'небо, cielo'],
+    ['친구', 'друг, amigo'],
+    ['우유', 'молоко, leche'],
+    ['뚱뚱하다', 'быть толстым, ser gorgo'],
+    ['사진', 'фотография, foto'],
+    ['사람', 'человек, persona'],
+    ['기다리다', 'ждать, esperar']
 ];
 
 var spanishWords = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+    ['madrugada', '새벽, рассвет'],
+    ['manzana', '사과, яблоко'],
+    ['amarillo', '노란, жёлтый'],
+    ['cascabel', '벨, колокол'],
+    ['hechizo', '마법, заклинание'],
+    ['anochecer', '저녁, наступит вечер'],
+    ['ojalá', '바라건대, надеюсь']
 ];
 
 $(document).ready(function randomize() {
 	var i = Math.floor(Math.random() * russianWords.length); //Random number
+    
 	$('#ru-word').append(russianWords[i][0]);
     $('#ru-definition').append(russianWords[i][1]);
-	//$('#kr-word').append(words[i][1]);
-	//$('#es-word').append(words[i][2]);
+    $('#kr-word').append(koreanWords[i][0]);
+    $('#kr-definition').append(koreanWords[i][1]);
+    $('#es-word').append(spanishWords[i][0]);
+    $('#es-definition').append(spanishWords[i][1]);
 
 	$('body').click(function(){
 		location.reload();
